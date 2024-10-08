faa61ba5fb7601a0dbf644d7b8b3f2c833a9f145
ThonyFM <137853537+ThonyFM@users.noreply.github.com>
2024-10-07 20:47:24 -0600

    Se crea scrip con la tabla de la base de datos y se crea el modelo de usuarios

---
+++ MatricuForms/Models/UsuarioModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatricuForms.Models
+{
+    public class UsuarioModel
+    {
+        #region atributos
+        public int ID { get; set; }
+        public String NICKNAME { get; set; }
+        public String CORREO { get; set; }
+        public String CONTRASENNA { get; set; }
+        public String ROL { get; set; }
+        public String ADICIONADOPOR { get; set; }
+        public DateTime FECHAADICION { get; set; }
+        public String MODIFICADOPOR { get; set; }
+        public DateTime FECHAMODIFICACION { get; set; }
+        #endregion
+
+    }
+}
